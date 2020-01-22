6a54488a3503009a3120de376a4af89332841c66
Tamizhvendan S <tamizh88@gmail.com>
2020-01-22 10:14:00 +0530

    fix pedastal sample issue

--- clojure/pedestal/src/server.clj
+++ clojure/pedestal/src/server.clj
@@ -5,18 +5,26 @@
             [com.walmartlabs.lacinia.pedestal :as lacinia-pedestal]
             [graphqlize.lacinia.core :as l]))
 
-(def ds-opts {:adapter           "postgresql"
-              :database-name     "sakila"
-              :server-name       "localhost"
-              :port-number       5432
-              :maximum-pool-size 1
-              :username          "postgres"
-              :password          "postgres"})
+(def db-spec (hikari/make-datasource {:adapter           "postgresql"
+                                      :database-name     "sakila"
+                                      :server-name       "localhost"
+                                      :port-number       5432
+                                      :maximum-pool-size 1
+                                      :username          "postgres"
+                                      :password          "postgres"}))
 
-(defn main- []
-  (with-open [db-spec (hikari/make-datasource ds-opts)]
-    (-> (heql-pg/initialize db-spec)
-        l/schema
-        (lacinia-pedestal/service-map {:graphiql true
-                                       :port     8080})
-        server/start)))
+(def heql-db-adapter (heql-pg/initialize db-spec))
+
+(def lacinia-schema (l/schema heql-db-adapter))
+
+(def service (lacinia-pedestal/service-map lacinia-schema {:graphiql true
+                                                           :port     8080}))
+
+(defonce runnable-service (server/create-server service))
+
+(.addShutdownHook (Runtime/getRuntime) (Thread. (fn []
+                                                  (server/stop runnable-service)
+                                                  (.close db-spec))))
+
+(defn -main []
+  (server/start runnable-service))
